8e910c08f22447f7d014f0b313b0f40864121539
Vladislav Vaganov <vladislav.vaganov@geniussports.com>
2022-01-05 20:50:05 +0200

    updated material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
 import React from 'react';
-import {Button, List, ListItem, ListItemText, Paper, Typography} from '@material-ui/core';
-import useAppStyles from './AppStyles';
+import {Box, Button, List, ListItem, ListItemText, Paper, Typography} from '@mui/material';
 
-export const App = (): React.ReactElement => {
-  const classes = useAppStyles();
+export const App = (): React.ReactElement => (
+  <Box
+    sx={{
+      display: 'flex',
+      boxSizing: 'border-box',
+      p: 2,
+      minHeight: '100vh',
+      width: '100%',
+      backgroundColor: '#f9fcfd',
+    }}
+  >
+    <Box sx={{height: '100%', width: '20%'}}>
+      <Typography variant="h6">Нумерология</Typography>
 
-  return (
-    <div className={classes.root}>
-      <div className={classes.sidebar}>
-        <Typography variant="h6">Нумерология</Typography>
+      <List component="nav">
+        <ListItem button>
+          <ListItemText primary="Люди" />
+        </ListItem>
+        <ListItem button>
+          <ListItemText primary="Даты" />
+        </ListItem>
+      </List>
+    </Box>
 
-        <List component="nav">
-          <ListItem button>
-            <ListItemText primary="Люди" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Даты" />
-          </ListItem>
-        </List>
-      </div>
+    <Paper sx={{ml: 2, flexGrow: 1, p: 2}}>
+      <Box sx={{m: 2}}>
+        <Button color="primary" variant="contained" onClick={() => alert('click')}>
+          Button
+        </Button>
+      </Box>
 
-      <Paper className={classes.content}>
-        <section className={classes.section}>
-          <Button color="primary" variant="contained" onClick={() => alert('click')}>
-            Button
-          </Button>
-        </section>
-
-        <section className={classes.section}>
-          <Typography display="inline">Text</Typography>
-        </section>
-      </Paper>
-    </div>
-  );
-};
+      <Box sx={{m: 2}}>
+        <Typography display="inline">Text</Typography>
+      </Box>
+    </Paper>
+  </Box>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CssBaseline} from '@mui/material';
 import {App} from './App';
 
 const appContainer = document.getElementById('appContainer');
-ReactDOM.render(<App />, appContainer);
+ReactDOM.render(
+  <>
+    <CssBaseline />
+    <App />
+  </>,
+  appContainer,
+);
