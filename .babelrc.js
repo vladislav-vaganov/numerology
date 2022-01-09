4da93abfcf055a758239bea29c15a2fc17139e20
Vladislav Vaganov <vladislav.vaganov@geniussports.com>
2022-01-09 22:07:57 +0200

    added personal energie chart

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,8 @@
 const plugins = [
-  ['babel-plugin-direct-import', {modules: ['@mui/material', '@mui/icons-material', '@mui/lab', 'date-fns']}],
+  [
+    'babel-plugin-direct-import',
+    {modules: ['@mui/material', '@mui/icons-material', '@mui/lab', 'date-fns', 'recharts']},
+  ],
 ];
 
 module.exports = {plugins};
--- src/PersonalEnergy.tsx
+++ src/PersonalEnergy.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Box, Typography} from '@mui/material';
+import {Box, Stack, Typography} from '@mui/material';
 import {isValidBirthDate} from './utils';
 import {NullableDate} from './types';
+import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts';
 
 const getEnergyNumbers = (birthDate: NullableDate): number[] => {
   if (!birthDate || !isValidBirthDate(birthDate)) {
@@ -52,14 +53,33 @@ export interface PersonalEnergyProps {
 export const PersonalEnergy = ({birthDate}: PersonalEnergyProps): React.ReactElement => {
   const energies = getEnergyNumbers(birthDate);
   const energiesByYears = groupEnergiesByYears(birthDate, energies, 101);
-  console.log(energiesByYears);
 
   return (
-    <Box>
-      <Typography display="inline">Личная энергия: </Typography>
-      <Typography display="inline" color="primary">
-        {energies.join(', ')}
-      </Typography>
-    </Box>
+    <Stack spacing={2}>
+      <Box>
+        <Typography display="inline">Личная энергия: </Typography>
+        <Typography display="inline" color="primary">
+          {energies.join(', ')}
+        </Typography>
+      </Box>
+
+      {!!energiesByYears.length && (
+        <Box sx={{overflowX: 'auto', width: '100%'}}>
+          <AreaChart width={4600} height={300} data={energiesByYears} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            <defs>
+              <linearGradient id="colorEnergie" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#00838f" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#00838f" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="year" interval={0} />
+            <YAxis scale="linear" domain={[0, 9]} interval={0} tickCount={9} />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Area type="monotone" dataKey="energie" stroke="#00838f" fillOpacity={1} fill="url(#colorEnergie)" />
+          </AreaChart>
+        </Box>
+      )}
+    </Stack>
   );
 };
