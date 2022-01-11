8a7171fdaf510e7b8d58aa753443f44aae5a7cc0
Vladislav Vaganov <vladislav.vaganov@geniussports.com>
2022-01-11 21:21:43 +0200

    sticky y-axis

--- src/EnergyByYearsChart.tsx
+++ src/EnergyByYearsChart.tsx
@@ -4,22 +4,19 @@ import {Area, AreaChart, CartesianGrid, ReferenceLine, Tooltip, XAxis, YAxis} fr
 import {CURRENT_YEAR} from './constants';
 
 const CURRENT_YEAR_LINE_ID = 'currentYearReferenceLine';
+const YEARS_COUNT = 101;
 
 interface EnergieByYear {
   year: number;
   energie: number;
 }
 
-const groupEnergiesByYears = (
-  birthYear: number | null,
-  energyNumbers: number[],
-  yearsCount: number,
-): EnergieByYear[] => {
-  if (!birthYear || energyNumbers.length < 7) {
+const groupEnergiesByYears = (birthYear: number | null, energyNumbers: number[]): EnergieByYear[] => {
+  if (!birthYear) {
     return [];
   }
 
-  return Array.from({length: yearsCount}, (_, i) => ({year: birthYear + i, energie: energyNumbers[i % 7]}));
+  return Array.from({length: YEARS_COUNT}, (_, i) => ({year: birthYear + i, energie: energyNumbers[i % 7]}));
 };
 
 const scrollChartToCurrentYear = (): void => {
@@ -32,7 +29,7 @@ interface EnergyByYearsChartProps {
 }
 
 export const EnergyByYearsChart = ({birthYear, energieNumbers}: EnergyByYearsChartProps): React.ReactElement | null => {
-  const energiesByYears = groupEnergiesByYears(birthYear, energieNumbers, 101);
+  const energiesByYears = groupEnergiesByYears(birthYear, energieNumbers);
 
   React.useEffect(() => {
     if (!energiesByYears.length) {
@@ -43,6 +40,16 @@ export const EnergyByYearsChart = ({birthYear, energieNumbers}: EnergyByYearsCha
 
   return !energiesByYears.length ? null : (
     <Box sx={{overflowX: 'auto', width: '100%'}}>
+      {/* hack: sticky y-axis emulation */}
+      <AreaChart
+        width={25}
+        height={277}
+        margin={{top: 10, right: 0, left: -35, bottom: 7}}
+        style={{position: 'absolute', marginLeft: -5, marginBottom: 23, backgroundColor: '#ffffff', zIndex: 1}}
+      >
+        <YAxis scale="linear" domain={[0, 9]} interval={0} tickCount={9} />
+      </AreaChart>
+
       <AreaChart width={4600} height={300} data={energiesByYears} margin={{top: 10, right: 30, left: -40, bottom: 0}}>
         <defs>
           <linearGradient id="colorEnergie" x1="0" y1="0" x2="0" y2="1">
