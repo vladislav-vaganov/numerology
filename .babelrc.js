59a3928c595f670eb8b74045646392ac4bf5e731
Vladislav Vaganov <vladislav.vaganov@geniussports.com>
2022-01-24 15:23:20 +0200

    got rid of babel-plugin-direct-import

--- .babelrc.js
+++
@@ -1,8 +0,0 @@
-const plugins = [
-  [
-    'babel-plugin-direct-import',
-    {modules: ['@mui/material', '@mui/icons-material', '@mui/lab', 'date-fns', 'recharts']},
-  ],
-];
-
-module.exports = {plugins};
